1cae33697376583b889ff0fa41085a457f4446e4
Nelson Daniel Ramirez <n.danielramirez.h@outlook.com>
2026-06-03 22:29:00 -0500

    initial commit

---
+++ src/main/java/com/dasther/nramirez/m5c7/M5c7Application.java
@@ -0,0 +1,13 @@
+package com.dasther.nramirez.m5c7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M5c7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M5c7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dasther/nramirez/m5c7/M5c7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasther.nramirez.m5c7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M5c7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
